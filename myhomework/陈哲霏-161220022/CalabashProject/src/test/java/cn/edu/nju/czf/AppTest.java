cdfe8ce5e21843f3389ae067205fdce371f555c6
Novel <novelist.chan@gmail.com>
2018-12-31 19:01:49 +0800

    陈哲霏-161220022

--- myhomework/陈哲霏-161220022/CalabashProject/src/test/java/cn/edu/nju/czf/AppTest.java
+++ myhomework/陈哲霏-161220022/CalabashProject/src/test/java/cn/edu/nju/czf/AppTest.java
@@ -1,9 +1,20 @@
 package cn.edu.nju.czf;
-
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import static org.junit.Assert.assertTrue;
 
+import board.ChessBoard;
+import creature.CalabashBro;
+import creature.CalabashManage;
+import creature.MonsterManage;
+import gui.BattlePhase;
+import gui.FileSystem;
+import gui.Main;
+import javafx.stage.Stage;
 import org.junit.Test;
-
+import creature.*;
+import board.*;
 /**
  * Unit test for simple App.
  */
@@ -13,8 +24,11 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testBeing(){
+        Being test = new Being("测试");
+        if(test.getName() != "测试"){
+            assertTrue("生成生物出错！", false);
+        }
+        assertTrue(true);
     }
 }
